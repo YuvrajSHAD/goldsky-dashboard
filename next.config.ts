227cb7649b39ebf78fa3cdb058ad3916b6d9423d
Yuvraj Singh <71546888+YuvrajSHAD@users.noreply.github.com>
2025-08-20 21:22:02 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
   images: {
-  domains: ['coin-images.coingecko.com'],
-}
+    domains: ['coin-images.coingecko.com'],
+  },
+  eslint: {
+    // Disable ESLint during production builds on Vercel
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
